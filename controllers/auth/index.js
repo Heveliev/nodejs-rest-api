4d2fab968bdb3b0bec7b62dd9fa8304395bd76ef
Anatolii Heveliev <109628656+Heveliev@users.noreply.github.com>
2023-06-18 15:53:36 +0300

    add verify user email

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -5,7 +5,10 @@ const login = require("./login");
 const getCurrent = require("./getCurrent");
 const logout = require("./logout");
 const updateSubscription = require("./updateSubscription");
-const changeAvatar = require("./changeAvatar")
+const changeAvatar = require("./changeAvatar");
+const verifyEmail = require("./verifyEmail");
+const resendVerifyEmail = require("./resendVerifyEmail");
+
 
 module.exports = {
     register: ctrlWrapper(register),
@@ -14,4 +17,6 @@ module.exports = {
     logout: ctrlWrapper(logout),
     updateSubscription: ctrlWrapper(updateSubscription),
     changeAvatar: ctrlWrapper(changeAvatar),
+    verifyEmail:ctrlWrapper(verifyEmail),
+    resendVerifyEmail:ctrlWrapper(resendVerifyEmail),
 }
--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,8 +1,9 @@
 const bcrypt = require("bcrypt");
-const {HttpError} = require("../../helpers");
+const {HttpError, sendEmail} = require("../../helpers");
 const {User} = require("../../models");
+const { nanoid } = require("nanoid");
 
-
+const { PROJECT_URL } = process.env;
 const gravatar = require('gravatar');
 
 const register = async(req,res) => {
@@ -15,12 +16,23 @@ const register = async(req,res) => {
 
     const avatarURL = gravatar.url(email, { s: '250', d: 'retro' });
 
-const hashPassword = await bcrypt.hash(password, 10)
-const newUser = await User.create({...req.body,avatarURL, password: hashPassword} );
-res.status(201).json({
-    email: newUser.email,
-    name: newUser.name,
-})
+    const hashPassword = await bcrypt.hash(password, 10);
+    const verificationCode = nanoid();
+
+
+    const newUser = await User.create({...req.body,avatarURL, password: hashPassword,verificationCode} );
+
+    const verifyEmail =  {
+        to: email,
+        subject: "Verify email",
+        html: `<a target="_blank" href="${PROJECT_URL}/users/verify/${verificationCode}">Click to verify email</a>`,
+      };
+    
+    await sendEmail(verifyEmail);
+    res.status(201).json({
+        email: newUser.email,
+        name: newUser.name,
+    })
 }
 
 
